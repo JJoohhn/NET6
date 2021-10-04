fc62fec5ac4663f4714a72b8c30bb9fb0e4ca9ae
Jan Halamka <Jan.Halamka@saint-gobain.com>
2021-10-04 15:23:23 +0200

    Hello

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,26 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-            PorovnaniCisel(6549, 5000);
+            Hello();
+            //const int c_a = 155;
+            //const int c_b = 255;
+
+            //var a = 25698;
+            //var b = 65478;
+
+            //if (a > b)
+            //    Console.WriteLine("cislo A: {0} je vetsi nez B: {1} ", a, b);
+            //else if (a == c_a)
+            //    return false;
+            //else (a == c_b)
+            //    false;
+
+            //static bool IsGreater(int a, int b)
+            //{
+            //    var x = (a > b) ? a : b;
+
+            //}
+            //PorovnaniCisel(6549, 5000);
             
 
            
