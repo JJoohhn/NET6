c92c92a7d95f8d8b8f35a65b7c90f7b90dc07233
Jan Halamka <Jan.Halamka@saint-gobain.com>
2021-10-04 13:40:09 +0200

    zadejSveJmeno

---
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+           
+            Hello();
+            Console.ReadLine();
+
+        }
+        static void Hello()
+        {
+            Console.Write("Zadej sve jmeno:");
+            string jmeno = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Hello {0}", jmeno);
+            
+        }
+
+
+    }
+}
