ae01f0f6c336c3fa714c90ea2cdc141e750066a5
Jan Halamka <Jan.Halamka@saint-gobain.com>
2021-10-05 09:27:37 +0200

    RokNarozeni

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -6,92 +6,29 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            PrevodTeploty();
-
-            //string input = Console.ReadLine();
-
-
-            //--------------------------
+            RokNarozeni();
             
-            //int a = 23;
-
-            //double d = 444.5684;
-
-            //a = (int)Math.Round(d);
-
-            //Console.WriteLine(a);
-            //--------------------------
-
-            //Hello();
-            //const int c_a = 155;
-            //const int c_b = 255;
-
-            //var a = 25698;
-            //var b = 65478;
-
-            //if (a > b)
-            //    Console.WriteLine("cislo A: {0} je vetsi nez B: {1} ", a, b);
-            //else if (a == c_a)
-            //    return false;
-            //else (a == c_b)
-            //    false;
-
-            //static bool IsGreater(int a, int b)
-            //{
-            //    var x = (a > b) ? a : b;
-
-            //}
-            //PorovnaniCisel(6549, 5000);
             
 
-           
-            //int a = 20;
-            //int b = 30;
-
-            //Console.WriteLine("a: {0}, b: {1}", a, b);
-
-            //int c = a;
-            //a = b;
-            //b = c;
-
-            //Console.WriteLine("a: {0}, b: {1}", a, b);
-
-           
-            //Hello();
-            //Console.ReadLine();
-
         }
-        
-        static void PorovnaniCisel (int a, int b)
+
+        static void RokNarozeni()
         {
-            if (a < b)
-                Console.WriteLine("Vetsi cislo je B: {0}", b);
-            else
-
-                Console.WriteLine("Vetsi cislo je A: {0}", a);
-        }
-        static void Hello()
-        {
-            Console.Write("Zadej sve jmeno:");
-            string jmeno = Console.ReadLine();
-            Console.WriteLine();
-            if (jmeno == "Bob")
-                Console.WriteLine("Vítej zpět Bobe.");
-            else if (jmeno == "Alice")
-                Console.WriteLine("Vítej zpět Alice.");
-            else
-                Console.WriteLine("Hello {0}", jmeno);
-
-        }
-        static void PrevodTeploty()
-        {
-            Console.Write("Zadej stupně ve Fahrenheit:");
-            double far = double.Parse(Console.ReadLine());
-            double cel = far - 112;
-            Console.WriteLine("Teplota v Celsia je: {0}", cel);
+            Console.WriteLine("napiš rok narození: ");
+            int rok = int.Parse(Console.ReadLine());
+            if (rok <= 1964 && rok >= 1946)
+                Console.WriteLine("Baby Boomerss");
+            if (rok <= 1980 && rok >= 1965)
+                Console.WriteLine("Generation X");
+            if (rok <= 1996 && rok >= 1981)
+                Console.WriteLine("Millenials");
+            if (rok <= 2012 && rok >= 1997)
+                Console.WriteLine("Generation Z");
+            if (rok >= 2013)
+                Console.WriteLine("Baby Boomerss");
 
         }
 
-        //Hello(name.ToLower()) -- Změna velikosti písma
+
     }
 }
---
+++ HelloWorld/HelloWorld/HelloWorld/TempCalcs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class TempCalcs
+    {
+        public static void PrintAndConvertTemp()
+        {
+            Console.WriteLine("zadej teplotu (F):");
+            var input_string = Console.ReadLine();
+            var input_double = double.Parse(input_string);
+            var c = ConvertFtoC(input_double);
+            Console.WriteLine($"Teplota v (C):{c}");
+        }
+
+        /// <summary>
+        /// Konertuje stupně
+        /// </summary>
+        /// <param name="f">jmeno f</param>
+        /// <returns></returns>
+        static double ConvertFtoC(double f)
+        {
+            return Math.Round(((f - 32) / 1.8), 2);
+        }
+        
+    }
+}
